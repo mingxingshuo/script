da24201a65aae3a40c35b9b6b40a1720b7263545
efdssdf <efdssdf@gmail.com>
2018-11-22 14:54:56 +0800

    'fix'

--- script/userInfo.js
+++ script/userInfo.js
@@ -1,8 +1,9 @@
 var schedule = require("node-schedule");
-var UserModel = require('../model/User');
+var UserconfModel = require('../model/UserconfModel');
 var ConfigModel = require('../model/Config');
 var getClient = require('../util/get_weichat_client');
 var async = require('async');
+var mem = require('../util/mem.js');
 
 function next_up(_id, code) {
     if (code) {
@@ -14,15 +15,18 @@ function next_up(_id, code) {
 }
 
 async function get_user() {
-    let configs = await ConfigModel.find()
-    console.log(configs,'--------------configs')
-    for(let config of configs){
-        update_user(null, config.code.toString(), next_up);
+    let configs = await ConfigModel.find({status: 1})
+    for (let config of configs) {
+        let updateUser = await mem.get("updateUser_" + config.code);
+        if(!updateUser){
+            update_user(null, config.code.toString(), next_up);
+        }
     }
 }
 
-function update_user(_id, code, next) {
-    UserModel.fetch_openid(_id, code, async function (error, users) {
+async function update_user(_id, code, next) {
+    await mem.set("updateUser_" + code, 1, 30 * 24 * 3600)
+    UserconfModel.fetch_openid(_id, code, async function (error, users) {
         var user_arr = [];
         users.forEach(function (user) {
             user_arr.push(user.openid)
@@ -36,10 +40,11 @@ function update_user(_id, code, next) {
                 if (err) {
                     console.log(err, '----------------nickname err1')
                 }
-                UserModel.findOneAndUpdate({openid: data.openid}, {
+                UserconfModel.findOneAndUpdate({openid: data.openid}, {
                     nickname: data.nickname,
                     headimgurl: data.headimgurl,
-                    sex:data.sex
+                    sex: data.sex,
+                    sign: 1
                 }, function (err, result) {
                     if (err) {
                         console.log(err)
@@ -48,38 +53,41 @@ function update_user(_id, code, next) {
                 return next(null, (parseInt(code) + 1).toString())
             })
         } else {
-            client.batchGetUsers(user_arr, function (err, data) {
+            client.batchGetUsers(user_arr, async function (err, data) {
                 if (err) {
                     console.log(err, '----------------nickname err2')
                     if (users.length == 50) {
                         return next(users[49]._id, code);
                     } else {
+                        await mem.set("updateUser_" + code, 0, 30 * 24 * 3600)
                         // return next(null, (parseInt(code) + 1).toString())
                     }
                 }
                 if (data && data.user_info_list) {
-                    async.eachLimit(data.user_info_list,10,function (info,callback) {
-                        if(info.nickname){
-                            UserModel.findOneAndUpdate({openid: info.openid}, {
+                    async.eachLimit(data.user_info_list, 10, function (info, callback) {
+                        if (info.nickname) {
+                            UserconfModel.findOneAndUpdate({openid: info.openid}, {
                                 nickname: info.nickname,
                                 headimgurl: info.headimgurl,
-                                sex:info.sex
+                                sex: info.sex,
+                                sign: 1
                             }, function (err, result) {
                                 if (err) {
                                     console.log(err)
                                 }
                                 callback(null)
                             });
-                        }else{
+                        } else {
                             callback(null)
                         }
-                    },function (error, result){
-                        if(error){
-                            console.log(error,'--------------error')
+                    }, async function (error, result) {
+                        if (error) {
+                            console.log(error, '--------------error')
                         }
                         if (users.length == 50) {
                             return next(users[49]._id, code);
                         } else {
+                            await mem.set("updateUser_" + code, 0, 30 * 24 * 3600)
                             // return next(null, (parseInt(code) + 1).toString())
                         }
                     })
@@ -89,11 +97,9 @@ function update_user(_id, code, next) {
     })
 }
 
-// get_user();
-
 var rule = new schedule.RecurrenceRule();
-var times = [1];
-rule.hour = times;
+var times = [1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56];
+rule.minute = times;
 var j = schedule.scheduleJob(rule, function () {
     console.log('更新用户信息');
     get_user();
