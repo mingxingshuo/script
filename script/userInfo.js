313397315220eca129ef295c89c57ce1c31220eb
efdssdf <efdssdf@gmail.com>
2018-12-11 15:04:15 +0800

    'fix'

--- script/userInfo.js
+++ script/userInfo.js
@@ -28,7 +28,7 @@ async function get_user() {
 async function update_user(_id, code, next) {
     await mem.set("updateUser_" + code, 1, 30 * 24 * 3600)
     UserconfModel.fetch_openid(_id, code, async function (error, users) {
-        console.log(users,'----------------------users')
+        console.log(users, '----------------------users')
         var user_arr = [];
         users.forEach(function (user) {
             user_arr.push(user.openid)
@@ -71,7 +71,7 @@ async function update_user(_id, code, next) {
                                 } else {
                                     arr0.push(info.openid)
                                 }
-                                console.log(info.sex,info.sex==1,arr1,'---------------arr1')
+                                console.log(info.sex, info.sex == 1, arr1, '---------------arr1')
                                 callback(null, arr0, arr1, arr2)
                             });
                         } else {
@@ -79,13 +79,13 @@ async function update_user(_id, code, next) {
                             callback(null, arr0, arr1, arr2)
                         }
                     }, async function (error, arr0, arr1, arr2) {
-                        console.log(arr0,arr1,arr2,'------------------arr')
+                        console.log(error, arr0, arr1, arr2, '------------------arr')
                         if (error) {
                             console.log(error, '--------------error')
                         }
-                        let people0 = await UserTagModel.findOne({code:code,name:"未知"})
-                        let people1 = await UserTagModel.findOne({code:code,name:"男"})
-                        let people2 = await UserTagModel.findOne({code:code,name:"女"})
+                        let people0 = await UserTagModel.findOne({code: code, name: "未知"})
+                        let people1 = await UserTagModel.findOne({code: code, name: "男"})
+                        let people2 = await UserTagModel.findOne({code: code, name: "女"})
                         client.membersBatchtagging(people0.id, arr0, function (error, res) {
                             console.log(res)
                         })
