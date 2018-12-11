782a35c20602f78878c91ea24cafe4a05b7fc881
efdssdf <efdssdf@gmail.com>
2018-12-11 15:06:15 +0800

    'fix'

--- script/userInfo.js
+++ script/userInfo.js
@@ -78,8 +78,8 @@ async function update_user(_id, code, next) {
                             arr0.push(info.openid)
                             callback(null, arr0, arr1, arr2)
                         }
-                    }, async function (error, arr0, arr1, arr2) {
-                        console.log(error, arr0, arr1, arr2, '------------------arr')
+                    }, async function (error, a, b, c) {
+                        console.log(error, a, b, c, '------------------arr')
                         if (error) {
                             console.log(error, '--------------error')
                         }
