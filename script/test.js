b9292d77751ea8ee3e23769da111a7f528702270
efdssdf <efdssdf@gmail.com>
2019-04-11 17:58:10 +0800

    'script'

--- script/test.js
+++ script/test.js
@@ -8,19 +8,19 @@ var wechat_util = require('../util/get_weichat_client.js')
 async function test(code) {
     // await RecordModel.update({_id:'5caf03cde2e9e277aa62d246'},{follow_openid:'oVWfp5goiCUf1IOidyepPmLyvEq0'})
     let client = await wechat_util.getClient(code)
-    // UserTagModel.remove({code: code}, function (err, doc) {
-    //     client.getTags(function (err, res) {
-    //         console.log(res, '------------------res')
-    //         for (let i of res.tags) {
-    //             console.log(i, '--------------------i')
-    //             if (i.name == "明星说男" || i.name == "明星说女" || i.name == "明星说未知") {
-    //                 client.deleteTag(i.id, function (error, res) {
-    //                     console.log(res)
-    //                 })
-    //             }
-    //         }
-    //     })
-    // })
+    UserTagModel.remove({code: code}, function (err, doc) {
+        client.getTags(function (err, res) {
+            console.log(res, '------------------res')
+            for (let i of res.tags) {
+                console.log(i, '--------------------i')
+                if (i.name == "明星说男" || i.name == "明星说女" || i.name == "明星说未知") {
+                    client.deleteTag(i.id, function (error, res) {
+                        console.log(res)
+                    })
+                }
+            }
+        })
+    })
     // client.createTag("明星说未知", async function (err, data) {
     //     client.createTag("明星说男", async function (err, data1) {
     //         client.createTag("明星说女", async function (err, data2) {
@@ -31,9 +31,9 @@ async function test(code) {
     //     })
     // })
 
-    client.getTags(function (err, data) {
-        console.log(data, '-----------------aaa')
-    })
+    // client.getTags(function (err, data) {
+    //     console.log(data, '-----------------aaa')
+    // })
     // let user_arr = ['oVWfp5goiCUf1IOidyepPmLyvEq0']
     // client.batchGetUsers(user_arr, async function (err, data) {
     //     console.log(err, data, '----------------------')
