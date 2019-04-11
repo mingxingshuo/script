09f3266b6f16a3b8c9d4d10aeeb17667ec22a208
efdssdf <efdssdf@gmail.com>
2019-04-11 15:52:28 +0800

    'fix'

--- script/test.js
+++ script/test.js
@@ -1,10 +1,13 @@
 var wechat_util = require('../util/get_weichat_client.js')
 
-async function test() {
-    let client = await wechat_util.getClient(225)
-    let user_arr = ['oVWfp5irPDm_FbVASNKS8Wbs9vDo','oVWfp5vz4RrH9riSItQR5VKPLHnk','x','xx']
-    client.batchGetUsers(user_arr, async function (err, data) {
-        console.log(err, data, '----------------------')
+async function test(code) {
+    let client = await wechat_util.getClient(code)
+    client.getTags(function (err,data) {
+        console.log(data,'-----------------aaa')
     })
+    // let user_arr = ['oVWfp5irPDm_FbVASNKS8Wbs9vDo','oVWfp5vz4RrH9riSItQR5VKPLHnk','x','xx']
+    // client.batchGetUsers(user_arr, async function (err, data) {
+    //     console.log(err, data, '----------------------')
+    // })
 }
-test()
+test(225)
