1f9e4854b732dc288f2263b5de82e5c18b83598c
efdssdf <efdssdf@gmail.com>
2019-04-11 16:56:55 +0800

    'fix'

--- routes/record_users.js
+++ routes/record_users.js
@@ -109,6 +109,7 @@ async function get_users(code, openid, callback) {
                             console.log('------------------------------');
                             return get_users(code, openid, callback);
                         }
+                        console.log(result.next_openid,result.data.openid,'----------------next')
                         if (result.next_openid) {
                             await RecordModel.findOneAndUpdate({code: code}, {
                                 follow_openid: result.next_openid,
