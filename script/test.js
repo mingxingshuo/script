8893f6586efe7f558dc75ebe73cbbe3d479c0a31
efdssdf <efdssdf@gmail.com>
2019-04-11 17:15:25 +0800

    'script'

--- script/test.js
+++ script/test.js
@@ -1,13 +1,31 @@
+var UserconfModel = require('../model/Userconf');
+var ConfigModel = require('../model/Config');
+var OpenidModel = require('../model/Openid');
+var UserTagModel = require('../model/UserTag')
+var RecordModel = require('../model/Record')
 var wechat_util = require('../util/get_weichat_client.js')
 
 async function test(code) {
     let client = await wechat_util.getClient(code)
+    UserTagModel.remove({code: code}, function (err, doc) {
+        client.getTags(function (err, res) {
+            console.log(res, '------------------res')
+            for (let i of res.tags) {
+                console.log(i, '--------------------i')
+                if (i.name == "明星说男" || i.name == "明星说女" || i.name == "明星说未知") {
+                    client.deleteTag(i.id, function (error, res) {
+                        console.log(res)
+                    })
+                }
+            }
+        })
+    })
     // client.getTags(function (err,data) {
     //     console.log(data,'-----------------aaa')
     // })
-    let user_arr = ['oVWfp5goiCUf1IOidyepPmLyvEq0']
-    client.batchGetUsers(user_arr, async function (err, data) {
-        console.log(err, data, '----------------------')
-    })
+    // let user_arr = ['oVWfp5goiCUf1IOidyepPmLyvEq0']
+    // client.batchGetUsers(user_arr, async function (err, data) {
+    //     console.log(err, data, '----------------------')
+    // })
 }
 test(225)
