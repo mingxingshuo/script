5f870068d3e7489ff0f8b13a0929f5d47f0bea0e
efdssdf <efdssdf@gmail.com>
2019-04-11 18:39:34 +0800

    'script'

--- script/test_tag.js
+++ script/test_tag.js
@@ -7,21 +7,23 @@ async function tag() {
     let code = process.argv.slice(2)[0]
     let tags = await UserTagModel.find({code: code})
     for (let tag of tags) {
-        console.log(tag,'------tag')
-        get_tag(null, code, tag.tagId, tag.sex)
+        console.log(tag, '------tag')
+        get_tag(null, code, tag.tagId, tag.sex, function (callback) {
+        })
     }
 }
 
-async function get_tag(_id, code, tagId, sex) {
-    console.log(code,'----------------code')
+async function get_tag(_id, code, tagId, sex, back) {
+    console.log(code, '----------------code')
     if (code) {
-        update_tag(_id, code, tagId, sex, get_tag);
+        update_tag(_id, code, tagId, sex, get_tag, back);
     } else {
         console.log('update_tag end');
+        back(null)
     }
 }
 
-function update_tag(_id, code, tagId, sex, next) {
+function update_tag(_id, code, tagId, sex, next, back) {
     UserconfModel.fetchTag(_id, code, sex, async function (error, users) {
         var user_arr = [];
         users.forEach(function (user) {
@@ -30,11 +32,11 @@ function update_tag(_id, code, tagId, sex, next) {
         let client = await wechat_util.getClient(code)
         if (user_arr.length == 0) {
             console.log(user_arr, '-------------------user null')
-            return next(null, null, null, null)
+            return next(null, null, null, null, back)
         } else {
             client.membersBatchtagging(tagId, user_arr, async function (error, res) {
                 if (error) {
-                    return next(_id, code, tagId, sex);
+                    return next(_id, code, tagId, sex.back);
                 }
                 if (res.errcode) {
                     await RecordModel.findOneAndUpdate({code: code}, {
@@ -42,7 +44,7 @@ function update_tag(_id, code, tagId, sex, next) {
                         tag_openid: user_arr[0],
                         errcode: res.errcode
                     }, {upsert: true})
-                    return next(null, null, null, null)
+                    return next(null, null, null, null, back)
                 }
                 await UserconfModel.remove({openid: {$in: user_arr}})
                 await RecordModel.findOneAndUpdate({code: code}, {
@@ -50,9 +52,9 @@ function update_tag(_id, code, tagId, sex, next) {
                     $inc: {tag_count: user_arr.length}
                 }, {upsert: true})
                 if (users.length == 50) {
-                    return next(users[49]._id, code, tagId, sex);
+                    return next(users[49]._id, code, tagId, sex, back);
                 } else {
-                    return next(null, null, null, null)
+                    return next(null, null, null, null, back)
                 }
             })
         }
