44335c06e5f3d5d8a8a818bcb0d16e5406df5171
efdssdf <efdssdf@gmail.com>
2019-04-15 14:15:13 +0800

    'fix'

--- util/get_weichat_client_shouzhan.js
+++ util/get_weichat_client_shouzhan.js
@@ -1,22 +1,22 @@
 // var weichat_conf = require('../conf/weichat.json');
-var ConfigModel = require('../model/Config');
+var ConfigModel = require('../model/Config_shouzhan');
 var WechatAPI = require('wechat-api');
 var Memcached = require('memcached');
 var memcached = new Memcached('127.0.0.1:11211');
 var mem = require('../util/mem.js');
 
 async function getClient(code) {
-    var config = await mem.get("configure_" + code);
+    var config = await mem.get("shouzhan_configure_" + code);
     if (!config) {
-        config = await ConfigModel.find({code: code})
-        config = config[0]
-        await mem.set("configure_" + code, config, 24 * 3600)
+        config = await ConfigModel.findOne({code: code})
+        console.log(config,'----------------------config')
+        await mem.set("shouzhan_configure_" + code, config, 24 * 3600)
     }
     // var config=weichat_conf[code];
     var api = new WechatAPI(config.appid, config.appsecret,
         function getToken(callback) {
             // console.log('----- getToken ----')
-            memcached.get('access_token' + code, function (err, token) {
+            memcached.get('shouzhan_access_token' + code, function (err, token) {
                 if (token) {
                     callback(null, JSON.parse(token));
                 } else {
@@ -26,7 +26,7 @@ async function getClient(code) {
         },
         function saveToken(token, callback) {
             // console.log('----- saveToken ----')
-            memcached.set('access_token' + code, JSON.stringify(token), 5 * 60, callback)
+            memcached.set('shouzhan_access_token' + code, JSON.stringify(token), 5 * 60, callback)
         });
     return api;
 }
