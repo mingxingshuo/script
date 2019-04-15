f1d1a511102f7702336ee38f2b5cbeb5d315418f
efdssdf <efdssdf@gmail.com>
2019-04-15 13:56:57 +0800

    'fix'

--- util/get_weichat_client.js
+++ util/get_weichat_client.js
@@ -6,29 +6,29 @@ var memcached = new Memcached('127.0.0.1:11211');
 var mem = require('../util/mem.js');
 
 async function getClient(code) {
-    var config = await mem.get("configure_"+code);
-    if(!config){
-        config = await ConfigModel.find({code:code})
-        config = config[0]
-        await mem.set("configure_"+code,config,30*24*3600)
+    var config = await mem.get("configure_" + code);
+    if (!config) {
+        config = await ConfigModel.findOne({code: code})
+        console.log(config,'----------------------config')
+        await mem.set("configure_" + code, config, 24 * 3600)
     }
-	// var config=weichat_conf[code];
-	var api = new WechatAPI(config.appid, config.appsecret,
-		function getToken(callback){
-			// console.log('----- getToken ----')
-			memcached.get('shouzhan_access_token'+code,function(err,token){
-				if(token){
-					callback(null,JSON.parse(token));
-				}else{
-					callback(null,null);
-				}
-			});
-		},
-		function saveToken(token,callback){
-			// console.log('----- saveToken ----')
-			memcached.set('shouzhan_access_token'+code,JSON.stringify(token),5*60,callback)
-		});
-	return api;
+    // var config=weichat_conf[code];
+    var api = new WechatAPI(config.appid, config.appsecret,
+        function getToken(callback) {
+            // console.log('----- getToken ----')
+            memcached.get('shouzhan_access_token' + code, function (err, token) {
+                if (token) {
+                    callback(null, JSON.parse(token));
+                } else {
+                    callback(null, null);
+                }
+            });
+        },
+        function saveToken(token, callback) {
+            // console.log('----- saveToken ----')
+            memcached.set('shouzhan_access_token' + code, JSON.stringify(token), 5 * 60, callback)
+        });
+    return api;
 }
 
 module.exports.getClient = getClient;
